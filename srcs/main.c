ebb727257748323ef8b647e17e0fafd14ef194e6
hnogared <hnogared@student.42perpignan.fr>
2023-11-10 18:04:56 +0100

    better main

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: hnogared <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/02 18:09:21 by hnogared          #+#    #+#             */
-/*   Updated: 2023/11/10 17:31:15 by hnogared         ###   ########.fr       */
+/*   Updated: 2023/11/10 18:03:18 by hnogared         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,11 @@
 int	main(int argc, char **argv)
 {
 	t_object	object;
-	t_coords	coords;
-	t_rgb_color	color;
 
 	if (argc == 2)
 		check_file(argv[1]);
-	coords.x = 1;
-	coords.y = 0;
-	coords.z = 10;
-	color.red = 110;
-	color.green = 0;
-	color.blue = 5;
-	new_sphere(&object, &coords, 1.0);
-	set_object_color(&object, &color);
+	new_sphere(&object, &(t_coords){1, 0, 10}, 1.0);
+	set_object_color(&object, &(t_rgb_color){110, 0, 5});
 	print_object_data(object);
 	return (0);
 }

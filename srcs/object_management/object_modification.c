24f13a57f9202849e7fa63f3272d07742bbd255d
hnogared <hnogared@student.42perpignan.fr>
2023-11-24 12:11:43 +0100

    better local axes calculation at object orientation modification

--- srcs/object_management/object_modification.c
+++ srcs/object_management/object_modification.c
@@ -6,7 +6,7 @@
 /*   By: hnogared <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/10 16:10:00 by hnogared          #+#    #+#             */
-/*   Updated: 2023/11/24 12:02:11 by hnogared         ###   ########.fr       */
+/*   Updated: 2023/11/24 12:06:48 by hnogared         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,22 +28,19 @@ t_object	*set_object_coords(t_object *to_set, t_coords new_coords)
 }
 
 /*
- * TODO comment i'm tred
- * TODO add normalise to z
+ * TODO comment i'm tired
  * https://math.stackexchange.com/questions/180418/calculate-rotation-matrix-to-align-vector-a-to-vector-b-in-3d 
  * https://en.wikipedia.org/wiki/Vector_projection#Vector_rejection
  */
 static t_local_axes	*set_local_axes(t_local_axes *to_set, t_vector orientation)
 {
-	float		vec_angle;
 	t_vector	y_axis;
 
 	if (!to_set)
 		return (NULL);
 	to_set->z = normalise(orientation);
 	y_axis = (t_vector){0, 1, 0};
-	vec_angle = prod_scal_vec(to_set->z, y_axis);
-	to_set->y = prod_vec_float(to_set->z, vec_angle);
+	to_set->y = prod_vec_float(to_set->z, prod_scal_vec(to_set->z, y_axis));
 	to_set->y = normalise(sous_vec_vec(y_axis, to_set->y));
 	to_set->x = normalise(prod_vec_vec(to_set->y, to_set->z));
 	return (to_set);

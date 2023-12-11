7779ae2701c34a198818fc18d71b85a45da5b768
Adrien Audebert <aaudeber@student.42perpignan.fr>
2023-12-11 08:24:20 +0100

    norm atof

--- srcs/libraries/extended_libft/Srcs/strings/ft_atof.c
+++ srcs/libraries/extended_libft/Srcs/strings/ft_atof.c
@@ -6,29 +6,33 @@
 /*   By: hnogared <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/03 13:49:59 by hnogared          #+#    #+#             */
-/*   Updated: 2023/12/05 11:42:46 by motoko           ###   ########.fr       */
+/*   Updated: 2023/12/11 08:18:20 by motoko           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-float	ft_atof(const char *nptr)
+static int	is_neg(const char *nptr)
 {
-	int		i;
-	int		sign;
-	float	nbr;
-	int		divider;
+	int	i;
 
 	i = 0;
-	nbr = 0.0;
-	sign = 1;
-	divider = 10;
-	while ((nptr[i] >= 9 && nptr[i] <= 13) || nptr[i] == 32)
-		i++;
 	if (nptr[i] == '-' || nptr[i] == '+')
 	{
 		if (nptr[i] == '-')
-			sign *= -1;
+			return (-1);
 		i++;
 	}
+	return (1);
+}
+
+static int	create_nbr(const char *nptr)
+{
+	int		divider;
+	int		i;
+	float	nbr;
+
+	i = 0;
+	nbr = 0.0;
+	divider = 10;
 	while (nptr[i] && nptr[i] >= '0' && nptr[i] <= '9')
 	{
 		nbr *= 10;
@@ -36,13 +40,21 @@ float	ft_atof(const char *nptr)
 		i++;
 	}
 	if (!nptr[i])
-		return (nbr * sign);
-	i++;
-	while (nptr[i] && (nptr[i] >= '0' && nptr[i] <= '9'))
+		return (nbr * is_neg(nptr));
+	while (nptr[++i] && (nptr[i] >= '0' && nptr[i] <= '9'))
 	{
 		nbr += (float)(nptr[i] - '0') / divider;
 		divider *= 10;
-		i++;
 	}
-	return (nbr * sign);
+	return (nbr);
+}
+
+float	ft_atof(const char *nptr)
+{
+	int		i;
+
+	i = 0;
+	while ((nptr[i] >= 9 && nptr[i] <= 13) || nptr[i] == 32)
+		i++;
+	return (create_nbr(nptr) * is_neg(nptr));
 }

6508173134ef85d5b2267a72fe1c244e5847f76c
hnogared <hnogared@student.42perpignan.fr>
2023-12-11 11:50:12 +0100

    improved raytrace function readability

--- srcs/raytracing/raytrace.c
+++ srcs/raytracing/raytrace.c
@@ -6,7 +6,7 @@
 /*   By: hnogared <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/06 11:01:02 by hnogared          #+#    #+#             */
-/*   Updated: 2023/12/11 11:28:10 by hnogared         ###   ########.fr       */
+/*   Updated: 2023/12/11 11:34:04 by hnogared         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,7 +45,8 @@ static t_rgb_color	get_reflections_color(t_ambient_light ambient_l, t_ray ray)
 	return (color);
 }
 
-t_rgb_color	rotated_raytrace(t_data *data, t_ray ray, float angle, t_vector axis)
+static t_rgb_color	rotated_raytrace(t_data *data, t_ray ray, float angle,
+	t_vector axis)
 {
 	ray.vector = axial_vector_rotation(ray.vector, angle, axis);
 	ray_advance(data, &ray);
@@ -58,23 +59,23 @@ t_rgb_color	rotated_raytrace(t_data *data, t_ray ray, float angle, t_vector axis
 size_t	raytrace(t_data *data, t_ray ray)
 {
 	size_t		new_rgb[3];
-	t_rgb_color	colors_list[5];
+	t_rgb_color	res_color;
 
-	colors_list[0] = rotated_raytrace(data, ray, 0.05f, ray.local_basis.y);
-	new_rgb[0] = colors_list[0].red;
-	new_rgb[1] = colors_list[0].green;
-	new_rgb[2] = colors_list[0].blue;
-	colors_list[1] = rotated_raytrace(data, ray, -0.05f, ray.local_basis.y);
-	new_rgb[0] += colors_list[1].red;
-	new_rgb[1] += colors_list[1].green;
-	new_rgb[2] += colors_list[1].blue;
-	colors_list[2] = rotated_raytrace(data, ray, 0.05f, ray.local_basis.z);
-	new_rgb[0] += colors_list[2].red;
-	new_rgb[1] += colors_list[2].green;
-	new_rgb[2] += colors_list[2].blue;
-	colors_list[3] = rotated_raytrace(data, ray, -0.05f, ray.local_basis.z);
-	new_rgb[0] += colors_list[3].red;
-	new_rgb[1] += colors_list[3].green;
-	new_rgb[2] += colors_list[3].blue;
+	res_color = rotated_raytrace(data, ray, 0.05f, ray.local_basis.y);
+	new_rgb[0] = res_color.red;
+	new_rgb[1] = res_color.green;
+	new_rgb[2] = res_color.blue;
+	res_color = rotated_raytrace(data, ray, -0.05f, ray.local_basis.y);
+	new_rgb[0] += res_color.red;
+	new_rgb[1] += res_color.green;
+	new_rgb[2] += res_color.blue;
+	res_color = rotated_raytrace(data, ray, 0.05f, ray.local_basis.z);
+	new_rgb[0] += res_color.red;
+	new_rgb[1] += res_color.green;
+	new_rgb[2] += res_color.blue;
+	res_color = rotated_raytrace(data, ray, -0.05f, ray.local_basis.z);
+	new_rgb[0] += res_color.red;
+	new_rgb[1] += res_color.green;
+	new_rgb[2] += res_color.blue;
 	return ((new_rgb[0] / 4) << 16 | (new_rgb[1] / 4) << 8 | new_rgb[2] / 4);
 }

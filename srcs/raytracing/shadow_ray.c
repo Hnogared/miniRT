c558d794ac03f2a5f3740850df92c3e0427882e4
hnogared <hnogared@student.42perpignan.fr>
2023-12-14 12:02:59 +0100

    added comments && normed

--- srcs/raytracing/shadow_ray.c
+++ srcs/raytracing/shadow_ray.c
@@ -6,17 +6,28 @@
 /*   By: hnogared <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/13 12:16:55 by hnogared          #+#    #+#             */
-/*   Updated: 2023/12/13 20:48:28 by hnogared         ###   ########.fr       */
+/*   Updated: 2023/12/14 11:49:52 by hnogared         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "miniRT.h"
 
 /*
- * Function 
+ * Function to check if the given shadow ray touches the light with the '@param
+ * light_id' inside the '@param objects_array' unobstructed by other objects.
+ * The ray won't be obstructed by other lights present in the scene.
+ * The pointed parameter ray's color will be updated if the light is reached.
+ *
+ * @param t_ray *shadow_ray				-> pointer to the ray to cast and modify
+ * @param t_object *objects_array		-> pointer to the scene objects to check
+ * @param unsigned short objects_count	-> amount of scene objects to check
+ * @param int light_id					-> tested light id in the objects pointer
+ * @return[redundant] t_rgb_color		-> the new calculated shadow ray color
+ *
+ * @parent_func shadow_ray	-> function to get the lighting of a point in space
  */
 static t_rgb_color	advance_shadow_ray(t_ray *shadow_ray,
-	t_object *objects_array, unsigned short objects_count, int light_id)
+	const t_object *objects_array, unsigned short objects_count, int light_id)
 {
 	int			i;
 	float		strength;
@@ -29,7 +40,6 @@ static t_rgb_color	advance_shadow_ray(t_ray *shadow_ray,
 			continue ;
 		do_touch(shadow_ray, objects_array[i], i);
 	}
-//	ft_printf("%d - %d\n", shadow_ray->go, light_id);
 	if (shadow_ray->go != light_id)
 		return (shadow_ray->light_color);
 	light = objects_array[shadow_ray->go];
@@ -39,7 +49,18 @@ static t_rgb_color	advance_shadow_ray(t_ray *shadow_ray,
 	return (shadow_ray->light_color);
 }
 
-t_rgb_color	shadow_ray(t_coords start_coords, t_object *objects_array,
+/*
+ * Function to get the lighting color of a point at '@param start_coords'
+ * according to the lights present in the given '@param objects_array'.
+ *
+ * @param t_coords start_coords			-> coords of the point in space to check
+ * @param t_object *objects_array		-> pointer to the scene objects to check
+ * @param unsigned short objects_count	-> amount of scene objects to check
+ * @return t_rgb_color					-> the resulting lighting color
+ *
+ * @child_func advance_shadow_ray	-> function to check if a ray touches a light
+ */
+t_rgb_color	shadow_ray(t_coords start_coords, const t_object *objects_array,
 	unsigned short objects_count)
 {
 	int		i;
@@ -55,8 +76,6 @@ t_rgb_color	shadow_ray(t_coords start_coords, t_object *objects_array,
 			continue ;
 		shadow_ray.vector = normalise(sous_vec_coord(objects_array[i].coords,
 					start_coords));
-//		shadow_ray.origin_coords = start_coords;
-//		shadow_ray.coords = start_coords;
 		shadow_ray.sol = -1;
 		shadow_ray.go = -1;
 		shadow_ray.res = 0;

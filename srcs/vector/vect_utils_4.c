c1446b43479c345e248c4b02292dd2b061603d3c
hnogared <hnogared@student.42perpignan.fr>
2023-12-18 15:07:50 +0100

    removed unnormed vect_utils_4.c comments

--- srcs/vector/vect_utils_4.c
+++ srcs/vector/vect_utils_4.c
@@ -6,7 +6,7 @@
 /*   By: tlorne <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/29 16:31:00 by tlorne            #+#    #+#             */
-/*   Updated: 2023/12/18 14:49:17 by hnogared         ###   ########.fr       */
+/*   Updated: 2023/12/18 15:07:24 by hnogared         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,9 +26,6 @@ t_coords	find_pos_touch(t_ray *ray, float t)
 {
 	t_coords	new_o;
 
-//	new_o.x = ray->origin_coords.x + (t - 0.1f) * ray->vector.x;
-//	new_o.y = ray->origin_coords.y + (t - 0.1f) * ray->vector.y;
-//	new_o.z = ray->origin_coords.z + (t - 0.1f) * ray->vector.z;
 	new_o.x = ray->origin_coords.x + t * ray->vector.x;
 	new_o.y = ray->origin_coords.y + t * ray->vector.y;
 	new_o.z = ray->origin_coords.z + t * ray->vector.z;
@@ -59,20 +56,6 @@ float	good_sol(float delta, float b, float a)
 	if (t1 >= 0)
 		return (t1);
 	return (t2);
-/*	if (t1 > 0 && t2 < 0)
-		return (t1);
-	else if (t1 < 0 && t2 > 0)
-		return (t2);
-	else if (t1 > 0 && t2 > 0)
-	{
-		if (t1 < t2)
-			return (t1);
-		else
-			return (t2);
-	}
-	else
-		return (0);
-*/
 }
 
 void	print_vec(t_vector vec)

a9ada5c572098f0e1c70cdd351d269ad72d497a8
hnogared <hnogared@student.42perpignan.fr>
2023-12-21 17:24:45 +0100

    moved the orthonormal basis generation functions into their file orthonormal_basis.c

--- srcs/object/object_modification.c
+++ srcs/object/object_modification.c
@@ -6,7 +6,7 @@
 /*   By: hnogared <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/10 16:10:00 by hnogared          #+#    #+#             */
-/*   Updated: 2023/12/15 23:47:19 by hnogared         ###   ########.fr       */
+/*   Updated: 2023/12/21 16:42:12 by hnogared         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,64 +25,6 @@ t_object	*set_object_coords(t_object *to_set, t_coords new_coords)
 		return (NULL);
 	to_set->coords = new_coords;
 	return (to_set);
-}
-
-/*
- * Function to calculate a 3D right-handed basis' orthogonal normal/unit vectors
- * with the @param vector as the x axis.
- *
- * @param t_vector xxx_orientation	-> the new basis' x axis orientation
- * @return t_basis					-> the newly calculated orthogonal basis
- */
-t_basis	get_ortho_basis_from_x(t_vector xxx_orientation)
-{
-	t_vector	z_axis;
-	t_basis		new;
-
-	new.x = normalise(xxx_orientation);
-	z_axis = (t_vector){0, 0, 1};
-	if (xxx_orientation.x == 0.0f && xxx_orientation.y == 0.0f
-		&& xxx_orientation.z == 1.0f)
-		new.z = (t_vector){0, -1, 0};
-	else if (xxx_orientation.x == 0.0f && xxx_orientation.y == 0.0f
-		&& xxx_orientation.z == -1.0f)
-		new.z = (t_vector){0, 1, 0};
-	else
-	{
-		new.z = prod_vec_float(new.x, prod_scal_vec(new.x, z_axis));
-		new.z = normalise(sous_vec_vec(z_axis, new.z));
-	}
-	new.y = normalise(prod_vec_vec(new.z, new.x));
-	return (new);
-}
-
-/*
- * Function to calculate a 3D right-handed basis' orthogonal normal/unit vectors
- * with the @param vector as the z axis.
- *
- * @param t_vector z_orientation	-> the new basis' z axis orientation
- * @return t_basis					-> the newly calculated orthogonal basis
- */
-static t_basis	get_ortho_basis_from_z(t_vector z_orientation)
-{
-	t_vector	y_axis;
-	t_basis		new;
-
-	new.z = normalise(z_orientation);
-	y_axis = (t_vector){0, 1, 0};
-	if (z_orientation.x == 0.0f && z_orientation.y == 1.0f
-		&& z_orientation.z == 0.0f)
-		new.y = (t_vector){0, 0, -1};
-	else if (z_orientation.x == 0.0f && z_orientation.y == -1.0f
-		&& z_orientation.z == 0.0f)
-		new.y = (t_vector){0, 0, 1};
-	else
-	{
-		new.y = prod_vec_float(new.z, prod_scal_vec(new.z, y_axis));
-		new.y = normalise(sous_vec_vec(y_axis, new.y));
-	}
-	new.x = normalise(prod_vec_vec(new.y, new.z));
-	return (new);
 }
 
 /*

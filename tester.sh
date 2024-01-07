f728071161f966fd09110822372ad1ab5f9a04cf
hnogared <hnogared@student.42perpignan.fr>
2024-01-07 17:45:31 +0100

    added brightness range testing

--- tester.sh
+++ tester.sh
@@ -1,3 +1,15 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    tester.sh                                          :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: hnogared <marvin@42.fr>                    +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2024/01/07 17:44:57 by hnogared          #+#    #+#              #
+#    Updated: 2024/01/07 17:45:03 by hnogared         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
 #!/bin/bash
 
 if [ "$1" == "1" ]; then
@@ -10,7 +22,13 @@ echo -e "\e[31m_PARSING_TESTER_\e[0m\n"
 
 echo -e "\n\e[32m_FILE_NAME_\e[0m\n"
 make -s
-file_syntax=("test1.rt" "test.rt.rt" "test.rt.blabla" ".rt.test.rt" "wqer.rt.qwer")
+
+file_syntax=("test1.rt"
+	"test.rt.rt"
+	"test.rt.blabla"
+	".rt.test.rt"
+	"wqer.rt.qwer")
+
 for file_name in "${file_syntax[@]}"; do
 	echo -n "$file_name : "
 	./miniRT "$file_name"
@@ -19,10 +37,12 @@ done
 
 echo -e "\e[32m_ERROR_MAP_COMMA_EVERYWHERE\e[0m\n"
 make -s
+
 file_syntax=("err_comma_everywhere_1.rt"
 	"err_comma_everywhere_2.rt"
 	"err_comma_everywhere_3.rt"
 	"err_comma_everywhere_4.rt")
+
 for file_name in "${file_syntax[@]}"; do
 	echo -n "$file_name : "
 	$VALGRIND ./miniRT "${ERROR_DIR}/${file_name}"
@@ -31,7 +51,17 @@ done
 
 echo -e "\e[32m_ERROR_MAP\e[0m\n"
 make -s
-file_syntax=("err_double_A.rt" "err_duplicate_C.rt" "err_empty_map.rt" "err_first_el.rt" "err_number.rt" "err_too_many_args_objects.rt" "err_color_range.rt" "err_vector_range.rt")
+
+file_syntax=("err_double_A.rt"
+	"err_duplicate_C.rt"
+	"err_empty_map.rt"
+	"err_first_el.rt"
+	"err_number.rt"
+	"err_too_many_args_objects.rt"
+	"err_color_range.rt"
+	"err_vector_range.rt"
+	"err_brightness_range.rt")
+
 for file_name in "${file_syntax[@]}"; do
 	echo -n "$file_name : "
 	$VALGRIND ./miniRT "${ERROR_DIR}/${file_name}"

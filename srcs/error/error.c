ee2312e813ea18ffb6e11ab94e46738a4c06e03c
hnogared <hnogared@student.42perpignan.fr>
2024-01-07 14:13:15 +0100

    added comments

--- srcs/error/error.c
+++ srcs/error/error.c
@@ -6,14 +6,27 @@
 /*   By: motoko <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/13 13:38:24 by motoko            #+#    #+#             */
-/*   Updated: 2024/01/06 23:53:10 by hnogared         ###   ########.fr       */
+/*   Updated: 2024/01/07 14:09:58 by hnogared         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "miniRT.h"
 
-int	ft_perror(const char *prefix, char *const error_tab[RTERR_COUNT],
-	int errnum)
+/*
+ * Function to either display an errno error message or a custom error message
+ * from the parameter error messages array depending on the parameter errnum.
+ * The error message is preceeded by the defined PROG_NAME and the parameter
+ * prefix if not NULL.
+ * errnum < FIRST_ERRVAL  : errno error message
+ * errnum >= FIRST_ERRVAL : custom error message from error_tab[RTERR_COUNT]
+ *
+ * @param const char *prefix	-> string to display before the error if not NULL
+ * @param char *const error_tab[RTERR_COUNT]	-> error messages array to read
+ * @param unsigned int errnum	-> the error number
+ * @return unsigned int			-> the used converted error number
+ */
+unsigned int	ft_perror(const char *prefix,
+	char *const error_tab[RTERR_COUNT], unsigned int errnum)
 {
 	ft_fprintf(STDERR_FILENO, PROG_NAME);
 	if (errnum < FIRST_ERRVAL)
@@ -28,6 +41,11 @@ int	ft_perror(const char *prefix, char *const error_tab[RTERR_COUNT],
 	return (errnum - 1 - FIRST_ERRVAL);
 }
 
+/*
+ * Function to initialize all the error messages inside the parameter array.
+ *
+ * @param char *error_tab[RTERR_COUNT]	-> strings array to initialize
+ */
 void	init_error_tab(char *error_tab[RTERR_COUNT])
 {
 	error_tab[0] = RTSUCCESS_MSG;
@@ -48,33 +66,4 @@ void	init_error_tab(char *error_tab[RTERR_COUNT])
 	error_tab[15] = RTERR_NUM_COMMA_MSG;
 	error_tab[16] = RTERR_NUM_DOT_MSG;
 	error_tab[17] = RTERR_OPEN_WIN_MSG;
-}
-
-void	ft_panic(const t_data *data, const char *prefix, int errnum)
-{
-	exit(ft_perror(prefix, data->error_tab, errnum));
-}
-
-/*
-void	free_parsing_and_exit(t_data *data, char ***block)
-{
-	int	i;
-
-	i = 0;
-	if (!block)
-		return ;
-	while (block[i])
-	{
-		free_str_tab(block[i]);
-		i++;
-	}
-	free(block);
-	exit(0);
-}
-*/
-
-void	err(const char *str)
-{
-	printf("%s\n", str);
-	exit(0);
 }
--- srcs/error/error.h
+++ srcs/error/error.h
@@ -6,13 +6,16 @@
 /*   By: hnogared <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/18 16:09:40 by hnogared          #+#    #+#             */
-/*   Updated: 2024/01/06 23:53:51 by hnogared         ###   ########.fr       */
+/*   Updated: 2024/01/07 14:12:20 by hnogared         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ERROR_H
 # define ERROR_H
 
+/* ************************************************************************** */
+/* defines                                                                    */
+/* ************************************************************************** */
 /* The program name to display in ft_perror */
 # ifndef PROG_NAME
 #  define PROG_NAME	"miniRT"
@@ -24,7 +27,6 @@
 /* Total number of errors. Increment to add more errors */
 # define RTERR_COUNT	18
 
-/* ************************************************************************** */
 /* Error codes to use for ft_perror */
 # define RTSUCCESS				256
 # define RTERR					257
@@ -50,7 +52,6 @@
 
 # define RTERR_OPEN_WIN			273
 
-/* ************************************************************************** */
 /* Error messages displayed by ft_perror */
 # define RTSUCCESS_MSG			"success"
 # define RTERR_MSG				"error"
@@ -77,8 +78,29 @@
 # define RTERR_OPEN_WIN_MSG		"couldn't open window"
 
 /* ************************************************************************** */
-int		ft_perror(const char *prefix, char *const error_tab[RTERR_COUNT],
-			int errnum);
-void	init_error_tab(char *error_tab[RTERR_COUNT]);
+/* error.c                                                                    */
+/* ************************************************************************** */
+/*
+ * Function to either display an errno error message or a custom error message
+ * from the parameter error messages array depending on the parameter errnum.
+ * The error message is preceeded by the defined PROG_NAME and the parameter
+ * prefix if not NULL.
+ * errnum < FIRST_ERRVAL  : errno error message
+ * errnum >= FIRST_ERRVAL : custom error message from error_tab[RTERR_COUNT]
+ *
+ * @param const char *prefix	-> string to display before the error if not NULL
+ * @param char *const error_tab[RTERR_COUNT]	-> error messages array to read
+ * @param unsigned int errnum	-> the error number
+ * @return unsigned int			-> the used converted error number
+ */
+unsigned int	ft_perror(const char *prefix,
+					char *const error_tab[RTERR_COUNT], unsigned int errnum);
+
+/*
+ * Function to initialize all the error messages inside the parameter array.
+ *
+ * @param char *error_tab[RTERR_COUNT]	-> strings array to initialize
+ */
+void			init_error_tab(char *error_tab[RTERR_COUNT]);
 
 #endif
